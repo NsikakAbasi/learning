6662f42ef0bcec955d790fdba67c0aafeeb18553
NsikakAbasi <ibangha@sheridancollege.ca>
2020-01-23 09:56:12 -0500

    Update testing.java

--- src/main/java/testing.java
+++ src/main/java/testing.java
@@ -13,5 +13,6 @@ public class testing {
     public static void main(String[] args){
         System.out.println("learning github, asdasdasdas ");
         System.out.println("is this right?");
+        System.out.println("wake me up inside");
     }
 }
